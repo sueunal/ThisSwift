4d096a3bc7abf9da491ffcd61a2e32a3b0649d5b
sueunal <doms@kakao.com>
2023-01-21 21:15:07 +0900

    day1-swift commit array vs dictionary

--- README.md
+++ README.md
@@ -5,4 +5,29 @@
 
 ### Day 1
 ## Array vs Dictionary 
+배열과 딕셔너리의 차이점
+
+배열 선언
+let arraylist = [Int]()
+or
+let arraylist1 : [String] = ["Apple","Banana","strawberry"]
+로 선언 가능 let으로 선언하였기 때문에 타입 변경 및 값 변경 불가능
+
+딕셔너리 선언
+let dictionary0 : [String:String] = ["Apple":"apple","Banana":"banana"]
+로 선언 가능
+배열과는 다르게 [String:String]으로 타입이 지정되어있는데
+key : value 를 뜻하며 key와 대응하는 value가 존재한다.
+
+
+차이점
+배열은 순서가 존재한다.
+0 1 2 3 4 .. n 으로 0부터 시작한다.
+
+딕셔너리는 순서가 존재하지 않고
+만약 이것을 반복문으로 표현하였을 경우 배열은 순서대로
+값이 나오게 되지만 딕셔너리는 순서와 상관없이 값이 나온다.
+
+
+
 
--- ThisSwift.playground/Contents.swift
+++ ThisSwift.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let arraylist0 : [String] = ["Apple","Banana","strawberry"]
+
+let dictionary0 : [String:String] = ["Apple":"apple","Banana":"banana","Strawberry":"strawberry"]
+
+// 데이터 접근방법의 차이
+
+// 인덱스 접근
+arraylist0[0]
+arraylist0[1]
+arraylist0[2]
+//arraylist0[3] 없는 인덱스 접근하여 에러 발생
+
+//key 값 접근
+dictionary0["Apple"]
+dictionary0["Banana"]
+dictionary0["Strawberry"]
+dictionary0["Coconut"] // 에러가 발생하지 않고 nil 을 반환
+// 정렬의 차이
+
+//순서 대로 나옴 배열은 순서가 존재하기 때문
+for item in arraylist0{
+	print(item)
+}
+print("\n")
+//데이터의 순서가 존재하지 않음
+for item in dictionary0{
+	print(item.value)
+}
