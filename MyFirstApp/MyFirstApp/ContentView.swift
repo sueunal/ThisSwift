487e59974d48815cf33324510e54269e9528d028
sueunal <doms@kakao.com>
2023-01-27 11:38:46 +0900

    git ignore

--- MyFirstApp/MyFirstApp/ContentView.swift
+++ MyFirstApp/MyFirstApp/ContentView.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button{
+            print("Clicked!!!")
+        } label: {
+            Text("Hello, sueun!")
+                .font(.title)
+                .fontWeight(.black)
+                .foregroundColor(Color.blue)
+                .padding()
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ MyFirstApp/MyFirstApp/FirstView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstView.swift
+//  MyFirstApp
+//
+//  Created by sueun kim on 2023/01/27.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirstView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstView()
+    }
+}
