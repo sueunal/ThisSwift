e546ddd1fc767cd9a760837e50a980a7de69b9da
sueunal <doms@kakao.com>
2023-01-27 10:45:50 +0900

    MyFirstApp_IOS Add

---
+++ MyFirstApp/MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by sueun kim on 2023/01/27.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
