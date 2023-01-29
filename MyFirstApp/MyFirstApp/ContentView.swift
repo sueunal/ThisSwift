d115c9d49476ec1e4f2c3192e1b6c4aaa59611a5
sueunal <36671600+sueunal@users.noreply.github.com>
2023-01-29 13:05:16 +0900

    git delete UserInterfaceState.xcuserstate

--- MyFirstApp/MyFirstApp/ContentView.swift
+++ MyFirstApp/MyFirstApp/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     let name = "Hi Sueun!"
     var body: some View {
-        VStack{
+        HStack{
             Text(name)
             Text(name)
             Text(name)
